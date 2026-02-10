5d6216e25b1c5d6712de84b7f5fffcec2dd7e127
NielsLazaroms <niels.lazaroms@live.nl>
2026-02-10 19:28:17 +0100

    Update `mn-dual-horizontal-image`: add host class, adjust styles for flexible height, and replace demo images.

--- projects/demo-app/src/app/dual-horizontal-image-demo/dual-horizontal-image-demo.ts
+++ projects/demo-app/src/app/dual-horizontal-image-demo/dual-horizontal-image-demo.ts
@@ -8,7 +8,7 @@ import { MnDualHorizontalImage, MnDualHorizontalImageTypes } from 'mn-angular-li
 })
 export class DualHorizontalImageDemo {
   images: MnDualHorizontalImageTypes[] = [
-    { id: '1', url: 'https://images.socialdeal.nl/img/dp-dynamicbanner-mobile-sd-awards-hotel.jpg', alt: 'Image 1' },
-    { id: '2', url: 'https://images.socialdeal.nl/img/dp-img3-hotel-cozykamer.jpg', alt: 'Image 2' },
+    { id: '1', url: 'https://www.wildkogel-arena.at/site/assets/files/17949/wildkogelarena_2024-7237.1920x0-sp.jpg', alt: 'Image 1' },
+    { id: '2', url: 'https://6hsport15.wordpress.com/wp-content/uploads/2016/01/skic3abn.jpg?w=617&h=411', alt: 'Image 2' },
   ];
 }
