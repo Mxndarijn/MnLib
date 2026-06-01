44f77094e2c59568e7cb2b4b128549d2391f50ab
Mxndarijn <merijn.gommeren@hotmail.com>
2026-06-01 22:58:45 +0200

    feat: add `onPageSizeChange` callback to `mn-table` for handling dropdown page size changes

--- projects/mn-angular-lib/src/lib/features/mn-table/mn-table.component.ts
+++ projects/mn-angular-lib/src/lib/features/mn-table/mn-table.component.ts
@@ -246,6 +246,7 @@ export class MnTable<T = any> implements OnInit, OnDestroy, DoCheck {
     this.currentPage = 1;
     this.applyPagination();
     this.cdr.markForCheck();
+    this.dataSource.onPageSizeChange?.(newSize);
   }
 
   get visiblePages(): number[] {
